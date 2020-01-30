635f501f409cf3426cb99f7ce6d705ff3e87103f
diego8arock <diegoa2000alt@gmail.com>
2020-01-29 19:28:47 -0500

    Modify autogun, bullet and muzzle

--- weapons/autogun/AutoGun.gd
+++ weapons/autogun/AutoGun.gd
@@ -1,82 +1,122 @@
 extends Node2D
 
+#Export
 export var bullet: PackedScene
-#Onready
+#Enums
+enum CHARACTER { PLAYER, ENEMY, ALLY }
+enum STATES { IDLE,  ATTACK }
+#OnReady
 onready var muzzle = $Muzzle
 onready var get_target = $GetTarget
 onready var fire_rate = $FireRate
-#Members
+#Internal Members
 var rotate_speed: float = 10.0
 var targets = []
 var target
-var m_attack_groups = [] setget set_attack_groups
+var current_state
+var body_entered
+var body_exited
+#External Members
+var em_attack_groups = [] setget set_attack_groups
+var em_character setget set_character
+var em_damage setget set_damage
 
 func _ready() -> void:
 	
+	Debug.add(name)
 	get_target.start()
+	current_state = STATES.IDLE
 
 func _process(delta: float) -> void:
 	
-	if target:
-		var target_direction = (target.global_position - global_position).normalized()
-		var current_direction = Vector2.RIGHT.rotated(global_rotation)
-		global_rotation = current_direction.linear_interpolate(target_direction, rotate_speed * delta).angle()
+	Debug.do(name, "state", STATES.keys()[current_state])
+	match current_state:
+		STATES.IDLE:
+			pass
+		STATES.ATTACK:
+			var target_direction = (target.global_position - global_position).normalized()
+			var current_direction = Vector2.RIGHT.rotated(global_rotation)
+			global_rotation = current_direction.linear_interpolate(target_direction, rotate_speed * delta).angle()
 
 func shoot() -> void:
 	
-	if(m_attack_groups == null):
-		push_error("No attack group assigned to gun")
+	assert(em_attack_groups && em_attack_groups.size() > 0)
 		
 	var spread = rand_range(-10.0, 10.0)
 	var new_bullet = bullet.instance()
 	var direction = Vector2(1, 0).rotated(muzzle.global_rotation + deg2rad(spread))
-	new_bullet.set_attack_groups(m_attack_groups)
+	new_bullet.set_attack_groups(em_attack_groups)
+	new_bullet.set_damage(em_damage)
 	new_bullet.shoot(muzzle.global_position, direction)	
 	new_bullet.global_scale = $Gun.global_scale
 	get_tree().root.add_child(new_bullet)
 
-func set_attack_groups(attack_groups) -> void:
-
-	m_attack_groups = attack_groups
-
-func _on_FireRate_timeout() -> void:	
-	
-	if target:
-		shoot()
-		fire_rate.start()
-
 func acquire_target() -> void:	
-
-	if !target: 
-		if targets.size() > 0:
-			target = targets[0]
-			target.connect("died", self, "remove_target")
-			fire_rate.start()	
+	
+	if targets.size() > 0:
+		target = targets[0]
+		target.connect("died", self, "on_Target_died")
+		fire_rate.start()	
+		current_state = STATES.ATTACK
+	else:
+		fire_rate.stop()
+		current_state = STATES.IDLE
 			
 func remove_target() -> void:
 	
 	targets.erase(target)
-	target.safely_delete()
 	target = null
-	acquire_target()
+	acquire_target()	
+	
+func target_detected() -> void:
+	
+	if Functions.is_object_in_any_group(body_entered, em_attack_groups):
+		targets.append(body_entered)
+		acquire_target()
+		
+func target_lost() -> void:
+	
+	if Functions.is_object_in_any_group(body_exited, em_attack_groups):
+		if targets.has(body_exited):
+			targets.erase(body_exited)
+			if body_exited == target:
+				target = null
+				acquire_target()
 
+#Events
 func _on_GetTarget_timeout() -> void:
 	
 	acquire_target()
-
-
+	
+func _on_FireRate_timeout() -> void:	
+	
+	shoot()
+	fire_rate.start()
+		
 func _on_AttackRange_body_entered(body: PhysicsBody2D) -> void:
 	
-	if Functions.is_object_in_any_group(body, m_attack_groups):
-		targets.append(body)
-		acquire_target()
-
+	body_entered = body
+	if not target:
+		target_detected()
 
 func _on_AttackRange_body_exited(body: PhysicsBody2D) -> void:
 	
-	if Functions.is_object_in_any_group(body, m_attack_groups):
-		if targets.has(body):
-			targets.erase(body)
-			if body == target:
-				target = null
-				acquire_target()
+	body_exited = body
+	target_lost()
+	
+func on_Target_died() -> void:
+	
+	remove_target()	
+			
+#setters & getters
+func set_attack_groups(attack_groups) -> void:
+
+	em_attack_groups = attack_groups
+
+func set_character(character) -> void:
+	
+	em_character = character
+	
+func set_damage(damage) -> void:
+	
+	em_damage = damage
--- weapons/bullet/Bullet.gd
+++ weapons/bullet/Bullet.gd
@@ -1,30 +1,35 @@
 extends KinematicBody2D
 
 export var speed : float = 20.0
-var m_attack_groups = [] setget set_attack_groups
+var em_attack_groups = [] setget set_attack_groups
 var velocity = Vector2()
+var em_damage: float setget set_damage
 
 func _physics_process(delta: float) -> void:
 	
 	var collision = move_and_collide(velocity)
 	if collision: 
 		var collider = collision.collider
-		if Functions.is_object_in_any_group(collider, m_attack_groups) and collider.has_node(Modules.HEALTH):
-			collision.collider.get_node(Modules.HEALTH).take_damage(10)
+		if Functions.is_object_in_any_group(collider, em_attack_groups) and collider.has_node(Modules.HEALTH):
+			collision.collider.get_node(Modules.HEALTH).take_damage(em_damage)
 		destroy_bullet()
 
 func shoot(p_position: Vector2, p_rotation: Vector2):
 	
-	if m_attack_groups == null:
-		push_error("No attack groups assigned to bullet")
+	assert(em_attack_groups && em_attack_groups.size() > 0)
+	assert(em_damage)
 		
 	global_position = p_position
 	global_rotation = p_rotation.angle()
 	velocity = p_rotation * speed
 
+func set_damage(damage: float) -> void:
+	
+	em_damage = damage
+
 func set_attack_groups(attack_groups = []) -> void:
 	
-	m_attack_groups = attack_groups
+	em_attack_groups = attack_groups
 
 func _on_VisibilityNotifier2D_screen_exited() -> void:
 	
