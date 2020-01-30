b42d4dfa9a836028f8f4677ecd558d3e3cec250d
diego8arock <diegoa2000alt@gmail.com>
2020-01-29 19:29:25 -0500

    Modify player

--- entities/player/Player.gd
+++ entities/player/Player.gd
@@ -2,7 +2,7 @@ extends KinematicBody2D
 
 onready var auto_gun = $AutoGun/Pivot
 
-var speed: float = 100
+var speed: float = 200
 var velocity: Vector2
 var rotation_speed = 15.0
 
@@ -11,6 +11,7 @@ func _ready() -> void:
 	Debug.add(name, true)
 	add_to_group(Groups.PLAYER)
 	auto_gun.set_attack_groups([Groups.ENEMY])
+	auto_gun.set_damage(5)
 	Events.connect("joystick_moved", self, "move_player")
 
 func _physics_process(delta: float) -> void:
