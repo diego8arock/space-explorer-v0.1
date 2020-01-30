d5caa00c9b7a98006bf55d0b5b1fe8fc54f56542
diego8arock <diegoa2000alt@gmail.com>
2020-01-29 19:29:40 -0500

    Modify test cases

--- tests/quit/TestQuit.gd
+++ tests/quit/TestQuit.gd
@@ -1,6 +1,11 @@
-extends Node
+extends Control
 
 func _input(event: InputEvent) -> void:
 	
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
+
+
+func _on_TouchScreenButton_pressed() -> void:
+	
+	get_tree().quit()
