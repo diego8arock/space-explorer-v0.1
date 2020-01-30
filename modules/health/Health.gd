f9a05c36226ac4886f5b2f215ee8aa8826d281d0
diego8arock <diegoa2000alt@gmail.com>
2020-01-29 19:28:21 -0500

    Modify health module

--- modules/health/Health.gd
+++ modules/health/Health.gd
@@ -1,17 +1,42 @@
 extends Node
 
+#Constants
 const DEFAULT_MAX_HEALTH: float = 10.0
+const DEFAULT_MIN_HEALTH: float = 0.0
+#OnReady
+onready var safe_delete = $SafeDeleteTimer
+#Events
+signal died()
+#External Members
+var em_health: float setget set_health, get_health
 
-var m_health: float setget set_health, get_health
+func _ready() -> void:
+	
+	em_health = DEFAULT_MAX_HEALTH
+	
+	if not owner.has_method("safe_delete"):
+		push_warning("owner %s as no safe_delete method" % owner.name)
+	if not owner.has_method("died"):
+		push_warning("owner %s as no died method" % owner.name)
+		
+	connect("died", owner, "died")
+	safe_delete.connect("timeout", owner, "safe_delete")
 
 func take_damage(damage: float) -> void:
 	
-	m_health -= damage	
+	em_health -= damage	
+	check_health()
 
 func set_health(health) -> void:
 	
-	m_health = health
+	em_health = health
 	
 func get_health() -> float:
 	
-	return m_health
+	return em_health
+	
+func check_health() -> void:
+	
+	if em_health <= DEFAULT_MIN_HEALTH:
+		emit_signal("died")
+		safe_delete.start()
