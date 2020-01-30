6341b5e39cb3555b2a4c5e38a06a37aadf2f677b
diego8arock <diegoa2000alt@gmail.com>
2020-01-29 19:29:09 -0500

    Modify attack ship

--- entities/enemies/attack_ship/AttackShip.gd
+++ entities/enemies/attack_ship/AttackShip.gd
@@ -1,22 +1,47 @@
 extends KinematicBody2D
 
+#Export
+export var debug: bool = false
+#OnReady
 onready var muzzle_left = $Muzzles/MuzzleLeft
 onready var muzzle_right = $Muzzles/MuzzleRight
-
-var attack_groups = [Groups.PLAYER, Groups.ALLY]
+onready var health = $Health
+onready var fire_rate = $FireRate
+onready var collider = $CollisionPolygon2D
+#Members
+var m_attack_groups = [Groups.PLAYER, Groups.ALLY]
+var m_damage = 10.0
 
 signal died()
 
 func _ready() -> void:
 	
-	Debug.add(name, true)
+	Debug.add(name, debug)
 	add_to_group(Groups.ENEMY)	
-	muzzle_left.set_attack_groups(attack_groups)
-	muzzle_right.set_attack_groups(attack_groups)
+	muzzle_left.set_attack_groups(m_attack_groups)
+	muzzle_left.set_damage(m_damage)
+	muzzle_right.set_attack_groups(m_attack_groups)
+	muzzle_right.set_damage(m_damage)
 	
-func take_damage() -> void:
+func take_damage(damage: float) -> void:
 	
-	Debug.do(name, "damage", 10)
+	Debug.do(name, "damage", damage)
+	health.take_damage(damage)
+	
+func died() -> void:
+	
+	Debug.do(name, "died", "called")
+	collider.disabled = true
+	fire_rate.stop()
+	set_process(false)
+	set_physics_process(false)
+	visible = false
+	emit_signal("died")
+	
+func safe_delete() -> void:
+	
+	Debug.do(name, "safe_delete", "called")
+	call_deferred("free")
 
 func _on_FireRate_timeout() -> void:
 	
