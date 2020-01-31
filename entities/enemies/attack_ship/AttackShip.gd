0bc6e7d7d3bd16c77d767f15b4cb18addc2729a1
Diego <diego.ochoa@globant.com>
2020-01-31 16:04:54 -0500

    Improve attack ship functinality

--- entities/enemies/attack_ship/AttackShip.gd
+++ entities/enemies/attack_ship/AttackShip.gd
@@ -3,11 +3,12 @@ extends KinematicBody2D
 #Export
 export var debug: bool = false
 #OnReady
-onready var muzzle_left = $Muzzles/MuzzleLeft
-onready var muzzle_right = $Muzzles/MuzzleRight
-onready var health = $Health
-onready var fire_rate = $FireRate
-onready var collider = $CollisionPolygon2D
+onready var n_muzzle_left = $Muzzles/MuzzleLeft
+onready var n_muzzle_right = $Muzzles/MuzzleRight
+onready var n_health = $Health
+onready var n_fire_rate = $FireRate
+onready var n_collider = $CollisionPolygon2D
+onready var n_aim_target = $AimTarget
 #Members
 var m_attack_groups = [Groups.PLAYER, Groups.ALLY]
 var m_damage = 10.0
@@ -18,32 +19,51 @@ func _ready() -> void:
 	
 	Debug.add(name, debug)
 	add_to_group(Groups.ENEMY)	
-	muzzle_left.set_attack_groups(m_attack_groups)
-	muzzle_left.set_damage(m_damage)
-	muzzle_right.set_attack_groups(m_attack_groups)
-	muzzle_right.set_damage(m_damage)
-	
+	n_muzzle_left.set_attack_groups(m_attack_groups)
+	n_muzzle_left.set_damage(m_damage)
+	n_muzzle_right.set_attack_groups(m_attack_groups)
+	n_muzzle_right.set_damage(m_damage)
+	n_aim_target.target_not_detected()
+	n_health.set_health(1000)
+		
 func take_damage(damage: float) -> void:
 	
 	Debug.do(name, "damage", damage)
-	health.take_damage(damage)
+	n_health.take_damage(damage)
 	
 func died() -> void:
 	
 	Debug.do(name, "died", "called")
-	collider.disabled = true
-	fire_rate.stop()
-	set_process(false)
-	set_physics_process(false)
-	visible = false
-	emit_signal("died")
+	Functions.set_object_ready_for_queue_free(self, [n_collider], [n_fire_rate], "died")
 	
 func safe_delete() -> void:
 	
 	Debug.do(name, "safe_delete", "called")
 	call_deferred("free")
+	
+func taget_detected() -> void:
+	
+	n_aim_target.taget_detected()
+	
+func target_not_detected() -> void:
+	
+	n_aim_target.target_not_detected()
+	
+func target_aimed() -> void:
+	 
+	n_aim_target.target_aimed()
 
 func _on_FireRate_timeout() -> void:
 	
-	muzzle_left.shoot()
-	muzzle_right.shoot()
+	n_muzzle_left.shoot()
+	n_muzzle_right.shoot()
+
+func _on_SelectArea_input_event(_viewport: Node, event: InputEvent, _shape_idx: int) -> void:
+	
+	if event is InputEventScreenTouch:
+		Events.emit_signal("target_selected", self)
+		Debug.do(name, "_on_SelectArea_input_event", "touch")
+		
+
+
+
