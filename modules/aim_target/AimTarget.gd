e3b59a39d834093a6822d8fcf7d5bd0e0e5502bd
Diego <diego.ochoa@globant.com>
2020-01-31 16:04:24 -0500

    Add aiming module

---
+++ modules/aim_target/AimTarget.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+onready var aimed = $Aimed
+onready var detected = $Detected
+
+func target_not_detected() -> void:
+	
+	aimed.visible = false
+	detected.visible = false
+
+func taget_detected() -> void:
+	
+	detected.visible = true
+	aimed.visible = false
+	
+func target_aimed() -> void:
+	
+	detected.visible = false
+	aimed.visible = true
