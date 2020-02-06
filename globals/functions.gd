45021a8120d094a3f46e47b9e2015953dd7f0379
Diego <diego.ochoa@globant.com>
2020-02-06 16:03:14 -0500

    Add group detection functionality

--- globals/functions.gd
+++ globals/functions.gd
@@ -1,5 +1,29 @@
 extends Node
 
+static func add_object_to_groups(object: Node2D, groups = []) -> void:
+	
+	for g in groups:
+		object.add_to_group(g)
+		
+func get_objects_from_groups(groups = []) -> Array:
+	
+	var all_objects = {}
+	for g in groups:
+		var nodes = get_tree().get_nodes_in_group(g)
+		for o in nodes:
+			if all_objects.has(o):
+				all_objects[o] = all_objects[o] + 1
+			else:
+				all_objects[o] = 1
+				
+	var objects = []
+	for o in all_objects:
+		if all_objects[o] == groups.size():
+			objects.append(o)
+		
+	return objects
+		
+
 static func is_object_in_any_group(object: Node2D, groups = []) -> bool:
 	
 	for g in groups:
--- globals/groups.gd
+++ globals/groups.gd
@@ -3,4 +3,6 @@ extends Node
 const PLAYER = "player"
 const ENEMY = "enemy"
 const ALLY = "ally"
+const LEADER = "leader"
+const FOLLOWER = "follower"
 
