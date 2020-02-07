5c0c897f5501d918abcb707cd450d77bce2135e1
Diego <diego.ochoa@globant.com>
2020-02-07 12:10:40 -0500

    Add missing methods

--- entities/enemies/cargo _ship/CargoShip.gd
+++ entities/enemies/cargo _ship/CargoShip.gd
@@ -5,11 +5,14 @@ onready var health = $Health
 onready var steering = $Steering
 onready var n_stats = $Stats
 onready var n_aim_target = $AimTarget
+onready var n_collider = $CollisionPolygon2D
 
 var start: Position2D
 var end: Position2D
 var velocity: Vector2
 var attack_range = 400.0
+
+signal died(object)
 
 func _ready() -> void:
 	
@@ -17,6 +20,7 @@ func _ready() -> void:
 	add_to_group(Groups.ENEMY)
 	add_to_group(Groups.LEADER)
 	n_aim_target.target_not_detected()
+	health.set_health(n_stats.health)
 	
 func _physics_process(delta: float) -> void:
 	
@@ -32,6 +36,16 @@ func _physics_process(delta: float) -> void:
 	Debug.do(name,"velocity", velocity)
 	move_and_slide(velocity)
 	global_rotation = steering.rotate_to(velocity, global_rotation, delta)
+
+func died() -> void:
+	
+	Debug.do(name, "died", "called")
+	Functions.set_object_ready_for_queue_free(self, [n_collider], [], "died")
+	
+func safe_delete() -> void:
+	
+	Debug.do(name, "safe_delete", "called")
+	call_deferred("free")	
 	
 func target_detected() -> void:
 	
