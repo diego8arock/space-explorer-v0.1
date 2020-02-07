e642122068baf31569d3878a352993cbdf29f274
Diego <diego.ochoa@globant.com>
2020-02-07 09:34:30 -0500

    Prepare Test Comboy scene

--- tests/convoy/TestComboy.gd
+++ tests/convoy/TestComboy.gd
@@ -2,3 +2,31 @@ extends Node2D
 
 export var cargo_ship: PackedScene
 export var attack_ship: PackedScene
+export var player: PackedScene
+export var ui: PackedScene
+
+var n_cargo_ship
+var n_attack_ships = []
+var n_player
+var n_ui
+
+
+func _ready() -> void:
+		
+	n_ui = ui.instance()
+	add_child(n_ui)
+	
+	n_player = player.instance()
+	add_child(n_player)
+	
+	n_cargo_ship = cargo_ship.instance()
+	n_cargo_ship.end = Position2D.new()
+	n_cargo_ship.end.global_position = Vector2.ZERO
+	add_child(n_cargo_ship)
+	
+	for i in range(2):
+		var new_attack_ship = attack_ship.instance()
+		n_attack_ships.append(new_attack_ship)
+		add_child(new_attack_ship)
+	
+	
