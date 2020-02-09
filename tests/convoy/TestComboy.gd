c8a03582bcd1e9a319ed9d1aca3565a26a36148d
Diego <diegoa2000alt@gmail.com>
2020-02-08 22:16:45 -0500

    Modify test scene

--- tests/convoy/TestComboy.gd
+++ tests/convoy/TestComboy.gd
@@ -12,16 +12,17 @@ var n_attack_ships = []
 var n_player: Node2D
 var n_ui: CanvasLayer
 
-var start_position = Vector2(2000, 2000)
-var end_position = Vector2(-5000, -5000)
+var end_position = Vector2(-55000, -55000)
 
 func _ready() -> void:
 		
-#	n_ui = ui.instance()
-#	add_child(n_ui)
-#
-#	n_player = player.instance()
-#	add_child(n_player)
+	n_ui = ui.instance()
+	add_child(n_ui)
+	
+	Debug.insert_debug()
+
+	n_player = player.instance()
+	add_child(n_player)
 	
 	n_cargo_ship = cargo_ship.instance()
 	n_cargo_ship.global_position = leader_start.get_leader_position()
@@ -30,16 +31,17 @@ func _ready() -> void:
 	n_cargo_ship.end = n_end_position
 	add_child(n_cargo_ship)
 	add_child(n_end_position)
-	var camera2d = Camera2D.new()
-	camera2d.current = true
-	n_cargo_ship.add_child(camera2d)
+#	var camera2d = Camera2D.new()
+#	camera2d.current = true
+#	n_cargo_ship.add_child(camera2d)
 	
 	var followers_poisition = leader_start.get_follower_position()
 	
-	for i in range(3):
+	for i in range(1):
 		var new_attack_ship = attack_ship.instance()
 		n_attack_ships.append(new_attack_ship)
 		new_attack_ship.global_position = followers_poisition[i]
+		new_attack_ship.debug = true
 		add_child(new_attack_ship)
 	
 	
