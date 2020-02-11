7beeba12f0e01383e8e458cccff2ecacb38bce63
Diego <diego.ochoa@globant.com>
2020-02-11 15:52:19 -0500

    Add patrol movement functionality

--- entities/enemies/attack_ship/AttackShip.gd
+++ entities/enemies/attack_ship/AttackShip.gd
@@ -23,15 +23,18 @@ var m_target: Node2D
 var m_crowd = []
 var m_leader: Node2D
 var m_rotation_speed: float
+var m_target_to_patrol: Vector2 = Vector2.ZERO
 #Enum
-enum STATE {IDLE, FOLLOW_LEADER, ARRIVE_AT_TARGET, PURSUIT_TARGET}
+enum STATE {IDLE, WANDER, FOLLOW_LEADER, ARRIVE_AT_TARGET, PURSUIT_TARGET, PATROL}
 var m_current_state = STATE.IDLE
 
 signal died(object)
+signal request_random_ttm(object)
 
 func _ready() -> void:
 	
 	Debug.add(name, debug)
+	n_steering.debug = debug
 	add_to_group(Groups.ENEMY)	
 	add_to_group(Groups.FOLLOWER)
 	n_muzzle_left.set_attack_groups(m_attack_groups)
@@ -42,22 +45,27 @@ func _ready() -> void:
 	n_muzzle_right.set_bullet(bullet)
 	n_aim_target.target_not_detected()
 	n_health.set_health(n_stats.health)
-	m_leader = Functions.get_objects_from_groups([Groups.LEADER, Groups.ENEMY])[0]
-	m_current_state = STATE.FOLLOW_LEADER
+	var objects = Functions.get_objects_from_groups([Groups.LEADER, Groups.ENEMY])
+	if objects.size() == 1:
+		m_leader = objects[0]
+		m_current_state = STATE.FOLLOW_LEADER
+	m_current_state = STATE.PATROL
 	
 	d_draw_attack_range = $AttackRange/CollisionShape2D.shape.radius
 	d_draw_follow_range = $FollowRange/CollisionShape2D.shape.radius
 	
 func _draw() -> void:
 	
-	if d_draw_follow_range:
-		draw_arc(Vector2.ZERO, d_draw_follow_range, deg2rad(0), deg2rad(360), 50, Color(0.9,0.7,0))
-	if d_draw_attack_range:
-		draw_arc(Vector2.ZERO, d_draw_attack_range, deg2rad(0), deg2rad(360), 50, Color(1,0,0))
+	if debug:
+		if d_draw_follow_range:
+			draw_arc(Vector2.ZERO, d_draw_follow_range, deg2rad(0), deg2rad(360), 50, Color(0.9,0.7,0))
+		if d_draw_attack_range:
+			draw_arc(Vector2.ZERO, d_draw_attack_range, deg2rad(0), deg2rad(360), 50, Color(1,0,0))
 		
 func _process(_delta: float) -> void:
 	
-	update()
+	if debug:
+		update()
 
 func _physics_process(delta: float) -> void:
 	
@@ -66,15 +74,34 @@ func _physics_process(delta: float) -> void:
 	match m_current_state:
 		STATE.IDLE:
 			pass
+		STATE.WANDER:
+			wander()
 		STATE.FOLLOW_LEADER:
 			follow_leader()
 		STATE.ARRIVE_AT_TARGET:
 			arrive_at_target()	
 		STATE.PURSUIT_TARGET:
 			pursuit_target()
+		STATE.PATROL:
+			patrol()
 
+	Debug.do(name, "m_velocity", m_velocity)
 	move_and_slide(m_velocity)
 	global_rotation = n_steering.rotate_to(m_velocity, global_rotation, delta, m_rotation_speed)
+
+func wander() -> void:
+	
+	m_velocity = n_steering.wander(
+		m_velocity,
+		n_movement.wander.cirlce_distance, 
+		n_movement.wander.circle_radius,
+		n_movement.wander.angle_change,
+		n_movement.wander.max_speed,
+		n_movement.wander.max_force,
+		n_movement.wander.mass		
+	)
+	
+	m_rotation_speed = n_movement.wander.rotation_speed
 	
 func follow_leader() -> void:
 	
@@ -126,6 +153,34 @@ func pursuit_target() -> void:
 		)
 		
 	m_rotation_speed = n_movement.pursuit_target.rotation_speed
+	
+func patrol() -> void:
+	
+	if m_target_to_patrol == Vector2.ZERO:
+		request_random_ttm()
+	else:
+		m_velocity = n_steering.arrive(		
+			m_velocity, 
+			m_target_to_patrol, 
+			global_position,
+			n_movement.arrive_at_target.max_speed,
+			n_movement.arrive_at_target.max_force,
+			n_movement.arrive_at_target.mass,
+			n_movement.arrive_at_target.arrival_distance
+			)
+		
+		m_rotation_speed = n_movement.arrive_at_target.rotation_speed
+		
+		if global_position.distance_to(m_target_to_patrol) < 100:
+			m_target_to_patrol = Vector2.ZERO
+	
+func request_random_ttm() -> void:
+	
+	emit_signal("request_random_ttm", self)
+	
+func set_random_ttm(ttm) -> void:
+	
+	m_target_to_patrol = ttm
 
 func take_damage(damage: float) -> void:
 	
@@ -182,10 +237,12 @@ func _on_AttackRange_body_entered(body: Node2D):
 func _on_AttackRange_body_exited(body: Node) -> void:
 	
 	if body == m_target:
-		m_current_state = STATE.FOLLOW_LEADER
+		m_current_state = STATE.ARRIVE_AT_TARGET
+		n_fire_rate.stop()
 
 func _on_FollowRange_body_exited(body: Node) -> void:
 	
 	if body == m_target:
 		m_target = null
-		m_current_state = STATE.FOLLOW_LEADER
+		m_current_state = STATE.PATROL
+
