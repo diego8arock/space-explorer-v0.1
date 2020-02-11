5e68b64283c59f2a167e0725b282c940d0f24fd9
Diego <diego.ochoa@globant.com>
2020-02-11 15:52:54 -0500

    Move scripts and add player stats

---
+++ stats/player/scripts/player_stats.gd
@@ -0,0 +1,16 @@
+extends Resource
+class_name PlayerStats
+
+export var max_health: float
+export var max_shield: float
+export var shield_recharge_rate: float
+export var shield_recharge_delay: float
+
+export var max_speed: float
+export var max_acceleration: float
+export var max_turning_speed: float
+
+export var max_damage: float
+export var max_attack_rage: float
+export var max_fire_rate: float
+export var max_accuarcy: float
